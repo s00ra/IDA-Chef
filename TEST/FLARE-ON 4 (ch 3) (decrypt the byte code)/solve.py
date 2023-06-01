6db3d8c27732f30f50c2c9bcdfe7b139a90610ef
s00ra <120357712+s00ra@users.noreply.github.com>
2023-06-02 00:30:45 +0300

    temp-design
    - upload an temp design and a POC

--- TEST/FLARE-ON 4 (ch 3) (decrypt the byte code)/solve.py
+++
@@ -1,8 +0,0 @@
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 2222))
-
-
-# the key range (0 - 0xff) cause it use "bl" register in operation 
-s.send(b"\xa2")
-print(s.recv(1024))
